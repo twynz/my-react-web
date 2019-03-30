3ebd2b39758343744e7b9edd7530dc513fcc21a8
twynz <wenyuforwes@163.com>
2019-03-30 14:02:16 +1300

    2019-03-29 Improve header component,
    Use redux to manage data, add search box in header.

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,32 +1,148 @@
 import React, {Component} from "react";
-import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
+import {Link} from "react-router-dom";
+import {connect} from 'react-redux';
 import {
     HeaderWrapper,
     Logo,
     Nav,
-    NavItem
+    NavItem,
+    SearchWrapper,
+    Navsearch,
+    SearchInfo,
+    SearchInfoItem,
+    SearchInfoList
 } from "./style";
+import axios from 'axios';
+
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    getSearchInfo() {
+        console.log('get search info called!');
+        const getSearchList = this.props.getSearchInfoListAction;
+        const pageList = [];
+
+        let focused = this.props.focused;
+        let list = this.props.list;
+        console.log('!!!!!'+this.props.list.size);
+        if(list.size === 0) {
+            getSearchList();
+        }
+        console.log('this list is '+list);
+
+
+
+        for(let i = 0;i<list.length;i++) {
+            pageList.push(
+                <SearchInfoItem key={list[i]}>
+                    {list[i]}
+                </SearchInfoItem>
+            );
+        }
+
+
+        console.log("list length" + pageList.length);
+        if (focused) {
+            return (
+                <SearchInfo>
+                    推荐列表：
+                    <SearchInfoList>
+                        {pageList}
+                    </SearchInfoList>
+                </SearchInfo>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    searchInfoHandleClick() {
+
+        console.log("search method called!");
+        this.props.changeFocusedState(true);
+    }
+
+    searchInfoHandleBlur() {
+        console.log("search method blur called!");
+        this.props.changeFocusedState(false);
+    }
+
     render() {
+        const { focused }= this.props;
+        console.log('render called focused is'+focused);
+
         return (
+
             <HeaderWrapper>
                 <Link to='/'>
                     <Logo/>
                 </Link>
                 <Nav>
-                    <NavItem>首页</NavItem>
-                    <NavItem>下载App</NavItem>
-                    <NavItem>登陆</NavItem>
+                    <NavItem className='left'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'>登陆</NavItem>
                     <NavItem className='right'>
                         <i className="iconfont">&#xe636;</i>
                     </NavItem>
                 </Nav>
+                <SearchWrapper>
+                    <Navsearch className={focused ? 'expand' : 'default'}
+                               onClick={this.searchInfoHandleClick.bind(this)}
+                               onBlur={this.searchInfoHandleBlur.bind(this)}>
+                    </Navsearch>
+                    <i className={focused ? 'focused iconfont' : 'iconfont'}>
+                        &#xe614;
+                    </i>
+
+                    {focused?this.getSearchInfo():null}
+
+                </SearchWrapper>
             </HeaderWrapper>
         );
     }
 
 }
 
-export default connect(null, null)(Header);
+
+
+const mapStateToProps = (state) => {
+    console.log('mapToState called');
+    return {
+        focused: state.getIn(['header', 'focused']),
+        list: state.getIn(['header','list'])
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getSearchInfoListAction() {
+            console.log('axios called!');
+            axios.get("/api/getSearchInfoList").then((res) => {
+                let originAxiosRes = res.data.data;
+                let result = [];
+
+                for(let i=0;i<5;i++) {
+                    result.push(originAxiosRes[i]);
+                }
+                const getInfoListAction =  {
+                    type: 'getSearchInfoList',
+                    data: result
+                };
+                dispatch(getInfoListAction);
+            }).catch((e) => {
+                console.log('error'+e);
+            });
+        },
+        changeFocusedState(focused) {
+            const changeFocused = {type: 'changeFocusedAction', focused: focused};
+            dispatch(changeFocused);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,10 +1,18 @@
 import {fromJS} from "immutable";
 
-const deafult = fromJS({
-    focused: false
+const s = fromJS({
+    focused: false,
+    list: []
 });
 
-export default (state = deafult, action) => {
-    console.log(this);
+export default (state = s , action) => {
+    if(action.type === 'getSearchInfoList') {
+        console.log('get search info list called');
+        return state.set('list',action.data);
+    }
+    if(action.type === 'changeFocusedAction') {
+        console.log('action'+action.focused);
+        return state.set('focused',action.focused);
+    }
     return state;
 }
