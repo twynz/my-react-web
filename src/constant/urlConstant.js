75048c99f893396cf5b4b78f6c55078aada3859f
twynz <wenyuforwes@163.com>
2019-07-05 16:06:20 +1200

    use env variable to load service address

--- src/constant/urlConstant.js
+++ src/constant/urlConstant.js
@@ -1,5 +1,5 @@
-const AUTH_SERVICE = 'http://localhost:10001';
-const CONTENT_SERVICE = 'http://localhost:10002';
+const AUTH_SERVICE = process.env.REACT_APP_AUTH_SERVICE===null?process.env.REACT_APP_AUTH_SERVICE:'http://localhost'+':10001';
+const CONTENT_SERVICE = process.env.REACT_APP_CONTENT_SERVICE===null?process.env.REACT_APP_AUTH_SERVICE:'http://localhost'+':10002';
 
 export const GET_TOKEN = AUTH_SERVICE+'/oauth/token';
 export const ADD_ARTICLE = CONTENT_SERVICE+'/api/article';
