35225e40e60fdaf5721e8b1087067400442b8a7c
twynz <wenyuforwes@163.com>
2019-07-09 22:42:48 +1200

    add http head

--- src/constant/urlConstant.js
+++ src/constant/urlConstant.js
@@ -1,5 +1,5 @@
-const AUTH_SERVICE = "13.237.125.223:10001";
-const CONTENT_SERVICE = "13.237.125.223:10002";
+const AUTH_SERVICE = "http://13.237.125.223:10001";
+const CONTENT_SERVICE = "http://13.237.125.223:10002";
 
 export const GET_TOKEN = AUTH_SERVICE+'/oauth/token';
 export const ADD_ARTICLE = CONTENT_SERVICE+'/api/article';
